1adfe54025e84576eaa803b5410cc54da07a8add
y_jono <pek9dkl@gmail.com>
2017-07-22 03:35:22 +0900

    Add: オペミスをしないように、起動シーケンスをスクリプト化した。

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# systemctl stop bluetooth
+systemctl stop ntp
+systemctl stop nmbd
+/etc/init.d/avahi-daemon stop
+
+ps lax | grep -- [-]20 | awk '{print $3}' | xargs sudo renice 0 -p
+
+nice -n -12 python3 pistol.py
